83f315e72befbf10eb2764906936f61b86db55ed
jon1996 <jonathanpascal5@gmail.com>
2022-12-27 17:43:09 +0000

    add linkList

--- main.c
+++ main.c
@@ -1,4 +1,66 @@
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+    int varibl;
+    struct Node* next;
+}Node;
+
+void insert_end(Node** root, int value)
+{
+    Node* new_node = malloc(sizeof(Node));
+    if(new_node == NULL)
+    {
+        exit(1);
+    }
+    new_node->next = NULL;
+    new_node->varibl = value;
+
+    if(*root == NULL)
+    {
+        *root = new_node;
+        return;
+    }
+
+    Node* current_variable = *root;
+    while(current_variable->next != NULL)
+    {
+        current_variable = current_variable->next;
+    }
+    current_variable->next = new_node;
+
+
+}
+
+void deallocate(Node** root)
+{
+    Node* current_variable = *root;
+    while(current_variable->next != NULL)
+    {
+        Node* auxiliary_variable = current_variable;
+        current_variable = current_variable->next;
+        free(auxiliary_variable);
+    }
+    *root = NULL;
+}
+
+void serialize(Node* root)
+{
+    FILE* file = fopen("text.txt","w");
+    if(file == NULL)
+    {
+        exit(1);
+    }
+    for(Node* current_variable = root; current_variable != NULL; current_variable = current_variable->next)
+    {
+        fprintf(file, "%d, ", current_variable->varibl);
+
+    }
+    fclose(file);
+}
+
 
 int main() {
     int n;
